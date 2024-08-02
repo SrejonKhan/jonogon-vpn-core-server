729cb37c1b825266811accdbffd76f8814d63dd5
shamiulalamupom <1hourprograms@gmail.com>
2024-08-02 17:54:23 +0200

    feat: uploading file using multer, multerS3

--- src/server.js
+++ src/server.js
@@ -1,10 +1,41 @@
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+
 const express = require("express");
 var cors = require("cors");
 
 const server = express();
+
 server.use(cors("*"));
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
+
+const spaceEndpoint = new aws.Endpoint(process.env.AWS_ENDPOINT);
+const s3 = new aws.S3({
+  endpoint: spaceEndpoint,
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.BUCKET_SPACE,
+    acl: "public-read",
+    key: (req, file, cb) => {
+      console.log(file);
+      cb(null, file.originalname);
+    },
+  }),
+}).array("upload", 1);
+
+server.post("/upload", (req, res, next) => {
+  upload(req, res, (err) => {
+    if (err) {
+      res.send(err);
+    }
+    res.sendStatus(200);
+  });
+});
 
 server.get("/", (req, res) => {
   res.send(
