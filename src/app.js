64bd53b2b5a9a096e9c7aa12b65abe1bbf47376f
Srejon Khan <srejonkhan79@gmail.com>
2024-08-06 20:04:29 +0600

    feat: add https server on prod env

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const server = require("./server");
+const https = require("https");
+const fs = require("fs");
 
 const port = process.env.PORT;
 
@@ -13,6 +15,20 @@ console.log(
 );
 
 const main = () => {
+  // https
+  if (process.env.NODE_ENV == "production") {
+    const options = {
+      key: fs.readFileSync(process.env.SSL_KEY_PATH),
+      cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+    };
+    const httpsServer = https.createServer(options, server);
+    httpsServer.listen(port, () => {
+      console.log(`Jonogon VPN Root Server is listening on port ${port}.`);
+    });
+    return;
+  }
+
+  // http server
   server.listen(port, () => {
     console.log(`Jonogon VPN Root Server is listening on port ${port}.`);
   });
